1e8c89c1f2d02649d235c6d3714813a800ba174f
Crowdbotics <team@crowdbotics.com>
2021-08-24 13:31:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dlf_30068Identifier",
+  url: "https://dlf_30068Identifier.botics.co",
+  api: "https://dlf_30068Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
